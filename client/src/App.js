ea17ad3b8f2d2b1ef47149a10b51a378ef6cff29
gooliajulia929 <juliadwyer93@gmail.com>
2021-12-18 00:36:20 -0500

    spacial experimentation

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,46 @@
 
 import './App.css';
 import Nav from './components/Nav';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [stars, setStars] = useState([])
+  const array = [];
+
+  useEffect(() => {
+    for (let i=0; i < 500 ; i++ ) {
+      array.push('.')
+    } setStars(array);
+  }, [])
+
+
   return (
     <div className="App">
       <Nav />
+      
+      {/* <div className='lined thin'>
+        <h4>- take out the trash</h4>
+        <h4>- do the dishes</h4>
+        <h4>- update resume</h4>
+      </div> */}
+              <div className='night-sky'>
+          {stars.map((star, idx) => 
+            <p key={idx} style={{top: `${Math.random()*100}vh`, right: `${Math.random()*400}vw`, fontSize: `${Math.random()*20}px`}} className='star'>{star}</p>
+          )}
+        </div>
+        <div className='slow' >
+          {stars.map((star, idx) => 
+            <p key={idx} style={{top: `${Math.random()*100}vh`, right: `${Math.random()*400}vw`, fontSize: `${Math.random()*20}px`}} className='star'>{star}</p>
+          )}
+        </div>
+        <div className='globe-container'>
+        <div className='night-sky-container'>
+      </div>
+      <div className='moon'>
+        <p className='moon'>once upon a time</p>
+      </div>
+        </div>
+
     </div>
   );
 }
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,8 +2,13 @@
 
 export default function Nav() {
     return (
-        <div>
-            <p>Nav</p>
+        <div className='nav'>
+            <p id='projects'>Projects.</p>
+            <p id='me'>Me!</p>
+            <p id='questions'>Questions?</p>
+            <p id='resume'>Resume...</p>
+            <p id='fun'>Just For Fun</p>
+            {/* <button className='lined thin'>click me</button> */}
         </div>
     )
 }
